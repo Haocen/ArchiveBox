a36f04deb14980e031e3600f213830d680ca06af
Haocen Xu <admin@haocen.me>
2020-03-07 01:30:14 -0500

    Allow purging exact link

--- archivebox/purge.py
+++ archivebox/purge.py
@@ -4,13 +4,13 @@ import re
 from argparse import ArgumentParser
 from os.path import exists, join
 from shutil import rmtree
-from typing import List
+from typing import (List, Pattern)
 
 from config import ARCHIVE_DIR, OUTPUT_DIR
 from index import (parse_json_links_index, write_links_index)
 
 
-def cleanup_index(regexes: List[str], proceed: bool, delete: bool) -> None:
+def cleanup_index(exact_link: str, regexes: List[str], proceed: bool, delete: bool) -> None:
     if not exists(join(OUTPUT_DIR, 'index.json')):
         exit('index.json is missing; nothing to do')
 
@@ -26,16 +26,22 @@ def cleanup_index(regexes: List[str], proceed: bool, delete: bool) -> None:
                 filtered.append((l, r))
                 break
         else:
-            remaining.append(l)
+            if url == exact_link:
+                filtered.append((l, exact_link))
+            else:
+                remaining.append(l)
 
     if not filtered:
         exit('Search did not match any entries.')
 
     print('Filtered out {}/{} urls:'.format(len(filtered), len(links)))
 
-    for link, regex in filtered:
+    for link, regex_or_exact_link in filtered:
         url = link['url']
-        print(' {url} via {regex}'.format(url=url, regex=regex.pattern))
+        print(' {url} via {match}'.format(
+            url=url,
+            match=regex_or_exact_link.pattern if isinstance(regex_or_exact_link, Pattern) else regex_or_exact_link
+        ))
 
     if not proceed:
         answer = input('Remove {} entries from index? [y/n] '.format(
@@ -57,10 +63,18 @@ def cleanup_index(regexes: List[str], proceed: bool, delete: bool) -> None:
 
 if __name__ == '__main__':
     p = ArgumentParser('Index purging tool')
-    p.add_argument(
+    g = p.add_mutually_exclusive_group(required=True)
+    g.add_argument(
+        'link',
+        nargs='?',
+        type=str,
+        help='URL matching exactly with this link will be removed from the index.'
+    )
+    g.add_argument(
         '--regex',
         '-r',
         action='append',
+        default=[],
         help='Regular expression matching URLs to purge',
     )
     p.add_argument(
@@ -79,7 +93,7 @@ if __name__ == '__main__':
     )
 
     args = p.parse_args()
-    if args.regex:
-        cleanup_index(args.regex, proceed=args.yes, delete=args.delete)
+    if args.regex or args.link:
+        cleanup_index(exact_link=args.link, regexes=args.regex, proceed=args.yes, delete=args.delete)
     else:
         p.print_help()
